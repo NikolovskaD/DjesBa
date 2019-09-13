d95bb8231b98bc1d308ea30774757d1642416494
NikolovskaD <dr.nikolovksa@yahoo.com>
2019-09-13 23:06:03 +0200

    Fix RecyclerView bug, Display List of Contacts

--- app/src/main/java/mk/ukim/finki/djesba/FindUserActivity.java
+++ app/src/main/java/mk/ukim/finki/djesba/FindUserActivity.java
@@ -1,5 +1,6 @@
 package mk.ukim.finki.djesba;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,6 +8,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.telephony.TelephonyManager;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -16,28 +25,96 @@ public class FindUserActivity extends AppCompatActivity {
     private RecyclerView.Adapter mUserListAdapter;
     private RecyclerView.LayoutManager mUserListLayoutManager;
 
-    ArrayList<UserObject> userList;
+    ArrayList<UserObject> contactList, userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_user);
+
+        contactList = new ArrayList<>();
         userList = new ArrayList<>();
 
         initializeRecyclerView();
+        getContactList();
     }
 
     private void getContactList(){
+        String ISOprefix = getCountryISO();
         Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
         while (phones.moveToNext()){
             String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String phone = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
+            phone = phone.replace(" ","");
+            phone = phone.replace("-","");
+            phone = phone.replace("(","");
+            phone = phone.replace(")","");
+
+            if (!String.valueOf(phone.charAt(0)).equals("+"))
+                phone = ISOprefix + phone;
+
             UserObject mContact = new UserObject(name,phone);
-            userList.add(mContact);
-            //mu javuvame na RecyclerView deka nesto se smenilo, za da znae da go prikaze
-            mUserListAdapter.notifyDataSetChanged();
+            contactList.add(mContact);
+
+            getUserDetails(mContact);
         }
+    }
+
+    private void getUserDetails(UserObject mContact) {
+        DatabaseReference mUserDB = FirebaseDatabase.getInstance().getReference().child("user");
+        Query query = mUserDB.orderByChild("phone").equalTo(mContact.getPhone());
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    String phone = "";
+                    String name = "";
+                    for (DataSnapshot childSnapshot: dataSnapshot.getChildren()){
+                        if (childSnapshot.child("phone").getValue() != null){
+                            phone = childSnapshot.child("phone").getValue().toString();
+                        }
+                        if (childSnapshot.child("name").getValue() != null){
+                            name = childSnapshot.child("name").getValue().toString();
+                        }
+
+                        UserObject mUser = new UserObject(name,phone);
+                        if (name.equals(phone)){
+                            for (UserObject mContactIterator : contactList){
+                                if (mContactIterator.getPhone().equals(mUser.getPhone())){
+                                    mUser.setName(mContactIterator.getName());
+                                }
+                            }
+                        }
+
+                        userList.add(mUser);
+
+                        //mu javuvame na RecyclerView deka nesto se smenilo, za da znae da go prikaze
+                        mUserListAdapter.notifyDataSetChanged();
+                        return;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    //iso - bukvite identifikatori za drzava
+    private String getCountryISO(){
+        String iso = null;
+
+        TelephonyManager telephonyManager = (TelephonyManager) getApplicationContext().getSystemService(getApplicationContext().TELEPHONY_SERVICE);
+        if (telephonyManager.getNetworkCountryIso() != null){
+            if (!telephonyManager.getNetworkCountryIso().toString().equals("")){
+                iso = telephonyManager.getNetworkCountryIso().toString();
+            }
+        }
+
+        return CountryToPhonePrefix.getPhone(iso);
     }
 
     private void initializeRecyclerView() {
