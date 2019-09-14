0dac2fb52ad2a92062237efb27692327e57545c6
Jelena <jelena.mikan95@gmail.com>
2019-09-14 21:48:51 +0200

    Add send message

--- app/src/main/java/mk/ukim/finki/djesba/ChatActivity.java
+++ app/src/main/java/mk/ukim/finki/djesba/ChatActivity.java
@@ -7,8 +7,17 @@ import mk.ukim.finki.djesba.Chat.MessageAdapter;
 import mk.ukim.finki.djesba.Chat.MessageObject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
     private RecyclerView mChat;
@@ -16,17 +25,45 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager mChatLayoutManager;
 
     ArrayList<MessageObject> messageList;
+
+    String chatID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        chatID = getIntent().getExtras().getString("chatID");
+
+        Button mSend = findViewById(R.id.send);
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+
         initializeRecyclerView();
+    }
+
+    private void sendMessage(){
+        EditText mMessage = findViewById(R.id.message);
+
+        if(!mMessage.getText().toString().isEmpty()){
+            DatabaseReference newMessageDb = FirebaseDatabase.getInstance().getReference().child("chat").child(chatID).push();
+            Map newMessageMap = new HashMap<>();
+            newMessageMap.put("text", mMessage.getText().toString());
+            newMessageMap.put("creator", FirebaseAuth.getInstance().getUid());
+
+            newMessageDb.updateChildren(newMessageMap);
+
+        }
+        mMessage.setText(null);
     }
 
     private void initializeRecyclerView() {
         messageList = new ArrayList<>();
-        mChat = findViewById(R.id.message);
+        mChat = findViewById(R.id.messageList);
         //to scroll seamlessly
         mChat.setNestedScrollingEnabled(false);
         mChat.setHasFixedSize(false);
